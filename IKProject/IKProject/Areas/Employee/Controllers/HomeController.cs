08369a5cab0f603f7eee991434237b41ddaf2e9d
ilginayaz <ilgindeniz.ayaz@bilgeadamboost.com>
2024-08-06 12:29:19 +0300

    areas olusturuldu.

---
+++ IKProject/IKProject/Areas/Employee/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKProject.Areas.Employee.Controllers
+{
+
+    [Area("Employee")]
+    [Authorize(Roles = "Employee")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
