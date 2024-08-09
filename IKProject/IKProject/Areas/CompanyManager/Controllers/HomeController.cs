267685963e516ea717cc105b2a365025f3f1c480
karahanhivda <hivda.krhn@gmail.com>
2024-08-09 09:08:49 +0300

    değişiklikler eklendi

--- IKProject/IKProject/Areas/CompanyManager/Controllers/HomeController.cs
+++ IKProject/IKProject/Areas/CompanyManager/Controllers/HomeController.cs
@@ -1,17 +1,133 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IKProject.Data.Concrete;
+using IKProject.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Newtonsoft.Json;
+using System.Text;
 
-namespace IKProject.Areas.CompanyManager.Controllers
+namespace IKProjectMVC.Areas.CompanyManager.Controllers
 {
-
     [Area("CompanyManager")]
-public class HomeController : Controller
+    public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public HomeController(HttpClient httpClient)
         {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var izinListesi = IzinleriGetir();
+            ViewBag.izinler = izinListesi;
             return View();
         }
 
 
+        public IActionResult IzinleriGetir()
+        {
+            var list = new List<string>();
+            return View(list);
+        }
+
+        //çalışanların bilgilerini getiren istek
+        public async Task<IActionResult> GetUser(string userId)
+        {
+            var response = await _httpClient.GetAsync($"http://localhost:5240/api/Yonetici/getUser?userId={userId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var user = JsonConvert.DeserializeObject<ApplicationUser>(content); //
+                return View(user);  
+            }
+            return NotFound("Personel bulunamadı.");
+        }
+
+        // yöneticinin çalışanları
+        public async Task<IActionResult> GetEmployees()
+        {
+            var response = await _httpClient.GetAsync("http://localhost:5240/api/Yonetici/getEmployees");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var employees = JsonConvert.DeserializeObject<List<ApplicationUser>>(content);
+                return View(employees);
+            }
+            else
+            {
+                return View("Personel Bulunamadı!");
+            }
+        }
+
+        // izinler listesi 
+        public async Task<IActionResult> IzinListesi()
+        {
+            var response = await _httpClient.GetAsync("http://localhost:5240/api/Yonetici/izinListesi");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var izinListesi = JsonConvert.DeserializeObject<List<IzinTipi>>(content);
+                return View(izinListesi);
+            }
+            else
+            {
+                return View("Tekrar deneyin!");
+            }
+        }
+
+        // yeni personel kayıt
+        [HttpPost]
+        public async Task<IActionResult> RegisterEmployee(CalisanRegisterModel registerModel)
+        {
+            var jsonContent = JsonConvert.SerializeObject(registerModel);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("http://localhost:5240/api/Yonetici/register", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("GetEmployees");
+            }
+            else
+            {
+                return View("Tekrar deneyin!");
+            }
+        }
+
+        // izin onaylama
+        [HttpPatch]
+        public async Task<IActionResult> OnaylaIzin(string izinId)
+        {
+            var response = await _httpClient.PatchAsync($"http://localhost:5240/api/Yonetici/izinOnayla?id={izinId}", null);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("IzinListesi");
+            }
+            else
+            {
+                return View("Tekrar deneyin!");
+            }
+        }
+
+        // izin reddetme
+        [HttpPatch]
+        public async Task<IActionResult> ReddetIzin(string izinId)
+        {
+            var response = await _httpClient.PatchAsync($"http://localhost:5240/api/Yonetici/izinReddet?id={izinId}", null);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("IzinListesi");
+            }
+            else
+            {
+                return View("Tekrar deneyin!");
+            }
+        }
     }
 }
---
+++ IKProject/IKProject/Data/Concrete/IzinTipi.cs
@@ -0,0 +1,40 @@
+﻿using IKProject.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace IKProject.Data.Concrete
+{
+    public class IzinTipi
+    {
+        public class IzinTipi
+        {
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "Başlangıç tarihi gereklidir.")]
+            public DateTime BaslangicTarihi { get; set; }
+
+            [Required(ErrorMessage = "Bitiş tarihi gereklidir.")]
+            public DateTime BitisTarihi { get; set; }
+
+            public decimal? IzinGunSayisi { get; set; }
+
+            [Required(ErrorMessage = "İstek yorumu gereklidir.")]
+            public string IstekYorumu { get; set; }
+
+            public OnayDurumu? OnayDurumu { get; set; }
+
+            public DateTime CreatedTime { get; set; } = DateTime.Now;
+            public DateTime? UpdatedTime { get; set; }
+            public DateTime? DeletedTime { get; set; }
+            public Status Status { get; set; } = Status.AwatingApproval;
+
+            [Required(ErrorMessage = "Kullanıcı ID'si gereklidir.")]
+            public string AppUserId { get; set; }
+
+            public virtual ApplicationUser ApplicationUser { get; set; }
+
+            [Required(ErrorMessage = "İzin tipi gereklidir.")]
+            public IzinTipi IzinTipi { get; set; }
+            public int IzinTipiId { get; set; }
+        }
+    }
+}
