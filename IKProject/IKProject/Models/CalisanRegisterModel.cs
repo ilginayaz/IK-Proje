2862908e9334fd58100ca9ed4d375919611a59b8
karahanhivda <hivda.krhn@gmail.com>
2024-08-12 23:53:33 +0300

    Personel ekle sayfası eklendi, tasarimi yapildi

--- IKProject/IKProject/Models/CalisanRegisterModel.cs
+++ IKProject/IKProject/Models/CalisanRegisterModel.cs
@@ -6,45 +6,45 @@ namespace IKProject.Models
 {
     public class CalisanRegisterModel
     {
-        [Display(Name = "Email Adresiniz")]
+        [Display(Name = "Email Adresi")]
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
         [DataType(DataType.EmailAddress)]
         [EmailAddress]
         public string Email { get; set; }
-        [Display(Name = "Şifreniz")]
+        [Display(Name = "Şifre")]
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
         [DataType(DataType.Password)]
         public string? Password { get; set; } = Guid.NewGuid().ToString();
-        [Display(Name = "Profil Fotoğrafınız")]
+        [Display(Name = "Profil Fotoğrafı")]
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
         public string ProfilePhoto { get; set; }
         //[PictureFileExtention]
         //public IFormFile? UploadPath { get; set; }
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
         [MaxLength(25), MinLength(2)]
-        [Display(Name = "Adınız")]
+        [Display(Name = "Ad")]
         public string Adi { get; set; }
         [MaxLength(25), MinLength(2)]
-        [Display(Name = "İkinci Adınız")]
+        [Display(Name = "İkinci Ad")]
         public string? IkinciAdi { get; set; }
         [MaxLength(25), MinLength(2)]
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
-        [Display(Name = "Soyadınız")]
+        [Display(Name = "Soyad")]
         public string Soyadi { get; set; }
         [MaxLength(25), MinLength(2)]
-        [Display(Name = "İkinci Soyadınız")]
+        [Display(Name = "İkinci Soyad")]
         public string? IkinciSoyadi { get; set; }
         [MaxLength(11), MinLength(11)]
-        [Display(Name = "Telefon Numaraniz")]
+        [Display(Name = "Telefon Numarası")]
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
         [DataType(DataType.PhoneNumber)]
         public string TelefonNumarasi { get; set; }
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
-        [Display(Name = "Doğum Tarihiniz")]
+        [Display(Name = "Doğum Tarihi")]
         [DataType(DataType.Date)]
         public DateOnly DogumTarihi { get; set; }
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
-        [Display(Name = "Doğum Yeriniz")]
+        [Display(Name = "Doğum Yeri")]
         public Sehirler DogumYeri { get; set; }
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
         [MaxLength(11, ErrorMessage = "TC Kimlik Numarası 11 haneli olmalıdır.")]
@@ -57,7 +57,7 @@ namespace IKProject.Models
         public Meslek Meslek { get; set; }
         [Required(ErrorMessage = "Girilmesi Zorunlu Alan.")]
         [MaxLength(100), MinLength(5)]
-        [Display(Name = "Adresiniz")]
+        [Display(Name = "Adres")]
         public string Adres { get; set; }
         public Cinsiyet Cinsiyet { get; set; }
         public IdentityResult? Result { get; set; }
